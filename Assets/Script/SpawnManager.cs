5c56b1afde64f779032e933ff6e05d6d91004204
AliR02 <alison.n.ramirez@lions.enc.edu>
2023-10-03 10:47:22 -0400

    added spawining and collision

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,46 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] stalker;
+
+    private float spawnRangeX = 140;
+    private float spawnRangeZ = 120;
+
+    private float starDelay = 5;
+    private float spawnInterval = 1.5f;
+
+    private PlayerController playerControllerScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //repeat spawning with interval
+        InvokeRepeating("SpawnRandomStalker", starDelay, spawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomStalker()
+    {
+        //Randomly generate animal Index and spawn position
+        if (playerControllerScript.gameOver == false)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, Random.Range(-spawnRangeZ, spawnRangeZ));
+            int stalkerIndex = Random.Range(0, stalker.Length);
+
+            Instantiate(stalker[stalkerIndex], spawnPos, stalker[stalkerIndex].transform.rotation);
+        }
+
+
+    }
+
+}
--- Assets/Script/StalkerFollow.cs
+++ Assets/Script/StalkerFollow.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class spider : MonoBehaviour
+
+
+public class StalkerFollow : MonoBehaviour
 {
+    public Transform Player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,7 @@ public class spider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        NavMeshAgent stalker = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        stalker.SetDestination(Player.position);
     }
 }
